1346cc9e7786ffba577cc1fbbb9507bf939aeafe
Divya R <kwrdivyaraj100@gmail.com>
2023-12-05 13:28:02 +0530

    Added Modal component with state management

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, Modal, Typography } from '@mui/material';
 
 function Test() {
+    const [open, setOpen] = useState(false);
     return (
-        <div>Test</div>
+        <div>
+            <Button onClick={() => setOpen(true)}>Open Modal</Button>
+            <Modal  open={open} onClose={() => setOpen(false)}>
+                <Box position="absolute" top="50%" left="50%">
+                    <Typography>It is a Modal</Typography>
+                    <Button variant='contained' onClick={() => setOpen(false)}>Click Me</Button>
+                </Box>
+            </Modal>
+        </div>
     )
 }
 
